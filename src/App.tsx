69ad081f0ed9e03ceef1dc6de5aabd00ad62d4ad
rojandahal <maxpanye60@gmail.com>
2025-06-03 15:56:25 +0545

    fix: custom event calendar ready to be shipped

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import Calendar from "./components/Calendar";
-import { badges, events, holidays, topBadge } from "./data/mockData";
+import CalendarComponent from "./components/Calendar";
+// import { badges, events, holidays, topBadge } from "./data/mockData";
+import { Badge, Event, Holiday, TopBadge } from "./types";
 
-function App() {
+interface CalendarProps {
+  events: Event[];
+  badges: Badge[];
+  holidays: Holiday[];
+  topBadges: TopBadge[];
+  weekendDays: number[];
+}
+
+function Calendar({
+  events,
+  badges,
+  holidays,
+  topBadges,
+  weekendDays
+}: CalendarProps) {
   return (
-    <Calendar
+    <CalendarComponent
       events={events}
       badges={badges}
       holidays={holidays}
-      topBadges={topBadge}
-      weekendDays={[0, 6]} // Sun = 0 to Sat = 6
+      topBadges={topBadges}
+      weekendDays={weekendDays} // Sun = 0 to Sat = 6
     />
   );
 }
 
-export default App;
+export default Calendar;
